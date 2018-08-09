130879b29b664bfdda228a46d22d6c6cea969fa2
HugoRoriz <hugororiz93@gmail.com>
2018-08-09 21:54:16 +0100

    Development Branch for future testing

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+// ANGULAR MODULES
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+// COMPONENT MODULES
+
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'ZU/homepage', component: HomepageComponent},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
