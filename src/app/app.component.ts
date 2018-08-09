f731138c13a2dcf5c0d60c412cd0ef301a39e36b
HugoRoriz <hugororiz93@gmail.com>
2018-08-09 18:56:30 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ZU';
+
+  videos = document.getElementsByTagName('video');
+  fraction = 0.8;
+
+  checkScroll() {
+    for (let i = 0; i < this.videos.length; i++) {
+
+      const video = this.videos[i];
+
+        const x = video.offsetLeft,
+              y = video.offsetTop,
+              w = video.offsetWidth,
+              h = video.offsetHeight, r = x + w, // Right
+              b = y + h; // Bottom
+
+        let visibleX, visibleY, visible;
+
+        visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+        visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+        visible = visibleX * visibleY / (w * h);
+
+        if (visible > this.fraction) {
+            video.play();
+        } else {
+            video.pause();
+        }
+
+        window.addEventListener('scroll', this.checkScroll, false);
+        window.addEventListener('resize', this.checkScroll, false);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+// ANGULAR MODULES
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+// COMPONENT MODULES
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'homepage', component: HomepageComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
